caad68aa681b3333d9eeef5df64182331acebfd9
陈 海刚 <329206444@qq.com>
2018-05-13 12:55:27 +0800

    提交一下

--- CHGAdapter/AppDelegate.m
+++ CHGAdapter/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-//#import "TestViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,9 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-//    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[TestViewController new]];
-//    [self.window makeKeyAndVisible];
+
     return YES;
 }
 
